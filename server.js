31d86f23a2f4a1359333666f98d1b2a2afebcf4b
Melvin Welton <nivlem65@gmail.com>
2022-02-06 13:51:41 -0600

    commit on main

--- server.js
+++ server.js
@@ -12,16 +12,17 @@ const PORT = 3001;
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
-
-  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
+  app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname,'./public/notes.html'));
+})
 
-//   app.use('/api', apiRoutes);
-//   app.use('/', htmlRoutes);
+app.get('/notes', (req, res) => {
+    res.json(notes);
+  });
+
 
 
 
