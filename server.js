585efdd3304796d9fde8ce0589446e240e4c55c4
Melvin Welton <nivlem65@gmail.com>
2022-02-06 10:11:18 -0600

    commit on main

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express();
+const PORT = 3001;
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  })
